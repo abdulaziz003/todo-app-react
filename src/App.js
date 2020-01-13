c87a9897fd1f24e4d01c7c6bcce3f964565fda50
AzoozMac <abdulaziz-003@hotmail.com>
2020-01-13 23:51:42 +0300

    todo app with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {title: 'Todo App', newTodo: '', todos: [{title: 'try', done: false}]}
+  }
+
+  formSubmitted(event){
+    event.preventDefault();
+    this.setState({
+      newTodo: '',
+      todos: [...this.state.todos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    })
+  }
+
+  newTodoChanged(event){
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
+  toggleTodoDone(event,index){
+    const todos = [...this.state.todos];// copy the array
+    todos[index] = {
+      ...todos[index],
+      done: event.target.checked // update done property on copied todo
+    };// copy the todo
+    this.setState({
+      todos
+    })
+  }
+
+  removeTodo(index){
+    const todos = [...this.state.todos];// copy the array
+    todos.splice(index, 1);
+    this.setState({
+      todos
+    })
+  }
+
+  allDone(){
+    const todos = this.state.todos.map(todo=>{
+      return {...todo, done: true}
+
+    });
+    this.setState({
+      todos
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.title }</h1>
+        
+        <p>{this.state.newTodo }</p>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input onChange={(event) => this.newTodoChanged(event)} type="text" id="newTodo" value={this.state.newTodo}/>
+          <button type="submit">Add</button>
+        </form>
+        <button onClick={()=> this.allDone()}>All Done</button>
+        <ul>
+          {this.state.todos.map((todo, index)=>{
+            return(  
+              <li key={todo.title}>
+                <input onChange={(event)=> this.toggleTodoDone(event, index)} type="checkbox" name="done" id="done" checked={todo.done}/>
+                <span style={{textDecoration: todo.done ? 'line-through' : 'inherit'}}>{todo.title}</span>
+                <button onClick={()=> this.removeTodo(index)}>Remove</button>
+              </li>
+            ) 
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
